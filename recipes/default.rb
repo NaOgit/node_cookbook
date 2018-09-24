9925f34cb313aca88c53e40513eb533fdfb4b74b
NaO <noukongka@spartaglobal.com>
2018-09-24 15:44:39 +0100

    Link to gitHub

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+include_recipe "nodejs"
+
+nodejs_npm "pm2" do
+  action :install
+end
+
+package "nginx" do
+  action :install
+end
+
+service "nginx" do
+  action [:enable, :start]
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 require 'chefspec'
 require 'chefspec/berkshelf'
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
+
+at_exit {ChefSpec::Coverage.report!}
+# report of all the test
+# touch is the report of how many test has been ran
